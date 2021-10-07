2ac3b8b34a705f146c9a1ca2098b45e3f2af95d3
ivanna777 <70724335+ivanna777@users.noreply.github.com>
2021-10-07 19:31:55 +0300

    first commit

--- model/index.js
+++ model/index.js
@@ -1,56 +1,22 @@
-const fs = require('fs/promises')
-const path = require('path')
-const { v4: uuidv4 } = require('uuid')
+const { Contact } = require('./schema')
 
-const contacts = require('./contacts.json')
-const filePath = path.join(__dirname, 'contacts.json')
+const listContacts = async () => Contact.find({})
 
-const listContacts = async () => contacts
+const getContactById = (id) => Contact.findById(id)
 
-const getContactById = async (contactId) => {
-  const contact = await contacts.find(contact => contact.id === contactId)
-  if (!contact) {
-    return null
-  }
-  return contact
-}
+const addContact = async (body) => await Contact.create(body)
 
-const addContact = async (body) => {
-  const newContact = { ...body, id: uuidv4() }
-  console.log(newContact)
-  contacts.push(newContact)
+const removeContact = (id) => Contact.findByIdAndDelete(id)
 
-  await fs.writeFile(filePath, JSON.stringify(contacts))
+const updateContact = (id, body) => Contact.findByIdAndUpdate(id, body)
 
-  return newContact
-}
-
-const updateContact = async (contactId, body) => {
-  const idx = contacts.findIndex(contact => contact.id === contactId)
-  if (idx === -1) {
-    return null
-  }
-  const updatedContact = { ...contacts[idx], ...body }
-  contacts[idx] = updatedContact
-  console.log(updatedContact)
-  await fs.writeFile(filePath, JSON.stringify(contacts))
-  return updatedContact
-}
-
-const removeContact = async (contactId) => {
-  const idx = contacts.findIndex(contact => contact.id === contactId)
-  if (idx === -1) {
-    return null
-  }
-  contacts.splice(idx, 1)
-  await fs.writeFile(filePath, JSON.stringify(contacts))
-  return 'success remove'
-}
+const updateStatusContact = (id, body) => Contact.findByIdAndUpdate(id, body)
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
+  removeContact,
   updateContact,
+  updateStatusContact
 }
--- validation/validation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi')
-
-const schema = Joi.object({
-  name: Joi.required(),
-  email: Joi.required(),
-  phone: Joi.required()
-})
-
-module.exports = schema
