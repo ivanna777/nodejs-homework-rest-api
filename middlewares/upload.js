ab64509742dd68d74632b35f578646c0bcfadf9e
ivanna777 <70724335+ivanna777@users.noreply.github.com>
2021-10-21 16:35:08 +0300

    hw5

---
+++ middlewares/upload.js
@@ -0,0 +1,21 @@
+const multer = require('multer')
+const path = require('path')
+
+const tmpDir = path.join(__dirname, '../', 'tmp')
+const uploadConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tmpDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+  limits: {
+    fileSize: 2048
+  }
+})
+
+const upload = multer({
+  storage: uploadConfig
+})
+
+module.exports = upload
